08e257b6fe6f5c20ad8951e9e33e5b5da07f72ea
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2022-10-24 16:01:01 -0400

    Better submodule handling

--- setup.sh
+++ setup.sh
@@ -2,16 +2,20 @@
 
 # Clone the code
 echo Cloning source...
-git clone --recurse-submodules https://github.com/ucf-tower-app/web /root/workspace/web
-git clone --recurse-submodules https://github.com/ucf-tower-app/mobile /root/workspace/mobile
+git clone https://github.com/ucf-tower-app/web /root/workspace/web
+git clone https://github.com/ucf-tower-app/mobile /root/workspace/mobile
 git clone https://github.com/ucf-tower-app/xplat /root/workspace/xplat
 
 # Install dependencies
 echo Installing dependencies...
 cd /root/workspace/web
+git submodule init
+git submodule update
 npm install
 
 cd /root/workspace/mobile
+git submodule init
+git submodule update
 npm install
 
 echo Done!
